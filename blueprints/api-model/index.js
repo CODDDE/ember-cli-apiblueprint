9b69e2042c02d57977fe3b191d2324fb75284519
Fabio Sanvido <fabio@coddde.com>
2018-02-08 18:27:03 +0100

    model blueprint

---
+++ blueprints/api-model/index.js
@@ -0,0 +1,78 @@
+/* eslint-env node */
+
+const fs = require('fs');
+const existsSync = require('exists-sync');
+const path = require('path');
+const EOL = require('os').EOL;
+const removeFromFile = require('../../lib/utilities/remove-from-file');
+
+const {
+  camelize,
+  capitalize,
+  dasherize
+} = require('ember-cli-string-utils');
+
+module.exports = {
+  description: '',
+
+  locals(options) {
+    // Return custom template variables here.
+    return {
+      capitalCamelizedModuleName: capitalize(camelize(options.entity.name)),
+      pluralizedModuleName: `${dasherize(options.entity.name)}s`,
+    };
+  },
+  
+  sectionTitle: '# Data Structures' + EOL,
+  
+  /**
+   * Import the created data structure into main apib file.
+   * Also import generated group in appropiated files.
+   *
+   * @param {*} options 
+   */
+  afterInstall(options) {
+    const mainFile = path.join('api-blueprints', 'index.apib');
+    if (!existsSync(mainFile)) {
+      return;
+    }
+    const capitalCamelizedModuleName = capitalize(camelize(options.entity.name));
+    const content = `<!-- include(api-models/${options.entity.name}.apib) -->`;
+    const groupContent = `# Group ${capitalCamelizedModuleName} ${EOL}<!-- include(api-groups/${options.entity.name}/${options.entity.name}.apib) --> ${EOL}`;
+    
+    return this.insertIntoFile(mainFile, EOL + this.sectionTitle)
+      .then(() => {
+        return this.insertIntoFile(mainFile, content, {
+          after: this.sectionTitle,
+        });
+      })
+      .then(() => {
+        return this.insertIntoFile(mainFile, groupContent, {
+          before: this.sectionTitle,
+        }) 
+      });
+  },
+  
+  afterUninstall(options) {
+    const mainFile = path.join('api-blueprints', 'index.apib');
+    if (!existsSync(mainFile)) {
+      return;
+    }
+    
+    const capitalCamelizedModuleName = capitalize(camelize(options.entity.name));
+    const content = `<!-- include(api-models/${options.entity.name}.apib) -->`
+    const groupContent = `# Group ${capitalCamelizedModuleName} ${EOL}<!-- include(api-groups/${options.entity.name}/${options.entity.name}.apib) --> ${EOL}`;
+    
+    return removeFromFile(mainFile, content)
+      .then(() => removeFromFile(mainFile, groupContent) )
+      .then((resultValue)=>{
+        const clearSection = resultValue.contents.indexOf('include(api-models') === -1;
+
+        if(clearSection){
+          return removeFromFile(mainFile, this.sectionTitle);
+        }
+        
+        return resultValue;
+      })
+  },
+};
