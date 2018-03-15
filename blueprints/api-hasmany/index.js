e88bf84ee2b5d6974f0eeda180242bca76aa80e2
alex.gonzalez <alex.gonzalez@agiledevers.com>
2018-03-15 13:54:42 +0100

    use ember-inflector to pluralize

--- blueprints/api-hasmany/index.js
+++ blueprints/api-hasmany/index.js
@@ -2,7 +2,7 @@
 const existsSync = require('exists-sync');
 const path = require('path');
 const EOL = require('os').EOL;
-var inflection = require('inflection');
+const emberInflector = require('ember-inflector');
 
 const {
   camelize,
@@ -10,13 +10,13 @@ const {
   dasherize
 } = require('ember-cli-string-utils');
 
-const { pluralize } = inflection;
+const { pluralize } = emberInflector;
 
 const indent = '    ';
 
 module.exports = {
   description: 'Adds a `hasMany` relationship to a model and the related calls to it´s group',
-  
+
   availableOptions: [
     { name: 'to', type: String, default: 'Not present, model name is required' },
     { name: 'linked', type: Boolean, default: false, description: 'Serialize has a link-related url in the source model',
@@ -35,7 +35,7 @@ module.exports = {
   locals(options) {
     // Return custom template variables here.
     const modelType = options.modeltype || options.entity.name;
-    
+
     return {
       targetModel: options.to,
       camelizedTarget: camelize(options.to),
@@ -61,7 +61,7 @@ module.exports = {
       },
     }
   },
-  
+
   beforeInstall(options) {
     const {
       entity: {
@@ -71,9 +71,9 @@ module.exports = {
       linked,
       required
     } = options;
-    
+
     const modelType = options.modeltype || options.entity.name;
-    
+
     const targetModelFile = path.join('api-blueprints', 'api-models', `${modelType}.apib`);
     const sourceModelFile = path.join('api-blueprints', 'api-models', `${model}.apib`);
     if (!existsSync(sourceModelFile) || !existsSync(targetModelFile)) {
@@ -84,16 +84,16 @@ module.exports = {
     const resourceName = pluralize(dasherize(name));
     const nestedUnder = pluralize(dasherize(model));
     const description = `All ${pluralize(camelize(name))} for this ${sourceModelName}`;
-    
+
     const relationshipSection = `## ${sourceModelName}${required ? 'RequiredRelationships' : 'OptionalRelationships'} (object)${EOL}`;
-    
+
     let content = `+ \`${resourceName}\` (object)${EOL}`;
     if (linked) {
       content += `${indent}+ links (object)${EOL}`
       content += `${indent}${indent}+ related: \`/api/v1/${nestedUnder}/1/relationships/${resourceName}\` (string, required) - ${description}`
     } else {
       content += `${indent}+ data (array[${targetModelName}Type]) - ${description}`
-    } 
+    }
 
     return this.insertIntoFile(sourceModelFile, content, {
       after: relationshipSection
@@ -105,7 +105,7 @@ module.exports = {
       })
     });
   },
-  
+
   afterInstall(options) {
     const {
       entity: {
@@ -113,7 +113,7 @@ module.exports = {
       },
       to: model,
     } = options;
-    
+
     const sourceModelGroup = path.join(
       'api-blueprints',
       'api-groups',
@@ -122,8 +122,8 @@ module.exports = {
     );
     const pluralizedModelName = pluralize(dasherize(name));
     let relationshipCalls = `${EOL}<!-- include(./relationships/${pluralizedModelName}.apib) -->`
-    
+
     return this.insertIntoFile(sourceModelGroup, relationshipCalls);
-    
+
   }
 };
